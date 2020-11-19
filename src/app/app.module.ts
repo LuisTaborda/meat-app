4c67684872705b6573e1cb8fc034f377a2c74409
Luis Taborda <luisgustavo.taborda@gmail.com>
2020-11-19 20:07:02 -0300

    Adicionando HTTP ao Serviço de Restaurantes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
-import {ROUTES} from './app.routes'
+import { ROUTES } from './app.routes'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Restaurant} from './restaurant/restaurant.model'
-import {RestaurantsService} from "./restaurants.service"
+import { Restaurant } from './restaurant/restaurant.model'
+import { RestaurantsService } from "./restaurants.service"
+
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html'
@@ -10,12 +11,12 @@ export class RestaurantsComponent implements OnInit {
 
   restaurants: Restaurant[]
 
-  constructor(private restaurantsService: RestaurantsService) { 
+  constructor(private restaurantsService: RestaurantsService) {
 
   }
 
   ngOnInit() {
-    this.restaurants = this.restaurantsService.restaurants()
+    this.restaurantsService.restaurants().subscribe(restaurants => this.restaurants = restaurants)
   }
 
 }
