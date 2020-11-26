ee99c0f9a62044d63a8c8fba46265b945f811072
Luis Taborda <luisgustavo.taborda@gmail.com>
2020-11-26 09:13:18 -0300

    Aplicando Conceitos de Módulos ( Angular )

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from "@angular/common";
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { OrderService } from "app/order/order.service";
+import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shooping-cart.service";
+import { RestaurantsService } from "app/restaurants/restaurants.service";
+import { InputComponent } from "./input/input.component";
+import { RadioComponent } from "./radio/radio.component";
+import { RatingComponent } from "./rating/rating.component";
+
+
+@NgModule({
+    declarations: [InputComponent, RadioComponent, RatingComponent],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule],
+    exports: [InputComponent, RadioComponent, RatingComponent, CommonModule, FormsModule, ReactiveFormsModule]
+})
+export class SharedModule {
+    static forRoot(): ModuleWithProviders{
+        return{
+            ngModule:SharedModule,
+            providers:[ShoppingCartService, RestaurantsService, OrderService]
+        }
+    }
+}
